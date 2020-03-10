42c9389985f505323ff835b39745d205fc85f442
ViktorYurkov <59891077+ViktorYurkov@users.noreply.github.com>
2020-03-10 18:20:08 +0200

    Add files via upload
    додав 3 завдання

---
+++ Lesson1_3a.js
@@ -0,0 +1,75 @@
+let data = $('table');
+var t = [];
+var hod = true;
+
+data.mousedown(function () {
+    let findClick = event.target;
+    if (findClick.tagName != 'TD') return;
+    if (hod) {
+        findClick.classList.add("krest");
+        hod = false;
+    } else {
+        findClick.classList.add("null");
+        hod = true;
+    }
+});
+data.mouseup(function () {
+
+    if (result("krest")) {
+        alert('Кінець гри . Виграв Chip');
+        end();
+        begin();
+    } else if (result("null")) {
+        alert('Кінець гри . Виграв Deil');
+        end();
+        begin();
+    } else if (!hod) {
+        step('.chip', '.deil');
+    } else {
+        step('.deil', '.chip');
+    };
+
+    function result(nameClass) {
+        var t = $('td').map(function () {
+            return (this.className == nameClass ? true : false);
+        }).toArray();
+
+        if (t[0] & t[1] & t[2] || t[0] & t[3] & t[6] || t[0] & t[4] & t[8] ||
+            t[1] & t[4] & t[7] || t[2] & t[5] & t[8] || t[3] & t[4] & t[5] ||
+            t[2] & t[4] & t[6] || t[6] & t[7] & t[8]) {
+            return true;
+        }
+        //console.log(t);
+        return false;
+    }
+});
+
+function begin() {
+    if (confirm("Грають Чіп і Дейл. Першим ходить Чіп?")) {
+        hod = true;
+        step('.deil', '.chip');
+    } else {
+        hod = false;
+        step('.chip', '.deil');
+    }
+}
+
+function step(gamer1, gamer2) {
+    $(gamer1).css({
+        "background-image": "url('Чип_и_Дейл.jpg')"
+    });
+    $(gamer2).css({
+        "background-image": "none"
+    });
+};
+
+function end() {
+    $('td').each(function () {
+        this.classList.remove("null");
+        this.classList.remove("krest");
+    });
+};
+
+$(function () {
+    begin();
+});
